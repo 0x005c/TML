83303953f3063767a74c71686775baa5cb1ac18a
0x005c <52650857+0x005c@users.noreply.github.com>
2020-05-02 16:52:16 +0900

    Add exp_to_string

--- exp.ml
+++ exp.ml
@@ -5,3 +5,10 @@ type exp =
   | IAdd of exp * exp
   | ISub of exp * exp
 
+let rec exp_to_string e =
+  match e with
+  | Int i -> string_of_int i
+  | Var s -> s
+  | Apply (e1,e2) -> "("^exp_to_string e1^" "^exp_to_string e2^")"
+  | IAdd (e1,e2) -> "("^exp_to_string e1^"+"^exp_to_string e2^")"
+  | ISub (e1,e2) -> "("^exp_to_string e1^"-"^exp_to_string e2^")"
--- main.ml
+++ main.ml
@@ -1,5 +1,9 @@
 open Parse
 open Printf
+open Exp
 
-let _ = Parse.top Lexer.token (Lexing.from_channel stdin) ;;
+let _ =
+let exp = Parse.top Lexer.token (Lexing.from_channel stdin) in
+let s = exp_to_string exp^"\n" in
+printf "%s" s
 
