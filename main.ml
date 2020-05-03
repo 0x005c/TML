4c180df7a107bf0bd97a3f956aea3a40c51d5790
0x005c <52650857+0x005c@users.noreply.github.com>
2020-05-03 09:47:31 +0900

    Fix indentation

--- main.ml
+++ main.ml
@@ -20,13 +20,13 @@ let rec show_assign a =
 ;;
 
 let _ =
-let exp = Parse.top Lexer.token (Lexing.from_channel stdin) in
-let t = infer exp in
-let es = exp_to_string exp in
-let ts =
-  match t with
-  | Some a -> type_to_string a
-  | None -> "(none)"
-in
-printf "exp: %s\ntype: %s\n" es ts
+  let exp = Parse.top Lexer.token (Lexing.from_channel stdin) in
+  let t = infer exp in
+  let es = exp_to_string exp in
+  let ts =
+    match t with
+    | Some a -> type_to_string a
+    | None -> "(none)"
+  in
+  printf "exp: %s\ntype: %s\n" es ts
 ;;
