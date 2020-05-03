a320efa2369a8fdd0bef6d2f051dcb495ec01e7b
0x005c <52650857+0x005c@users.noreply.github.com>
2020-05-04 08:23:42 +0900

    Clean up main.ml

--- main.ml
+++ main.ml
@@ -1,30 +1,28 @@
-open Parse
 open Printf
-open Exp
-open Infer
-open Type
 
 let unify_check t1 t2 =
-  printf "unify %s %s\n" (type_to_string t1) (type_to_string t2);
+  printf "unify %s %s\n" (Type.type_to_string t1) (Type.type_to_string t2);
   let asgn = Infer.unify [(t1,t2)] in
   printf "unified %s %s\n"
-      (type_to_string (Infer.assign asgn t1))
-      (type_to_string (Infer.assign asgn t2))
+      (Type.type_to_string (Infer.assign asgn t1))
+      (Type.type_to_string (Infer.assign asgn t2))
 ;;
 
 let rec show_assign a =
   match a with
-  | Set [] -> printf ""
-  | Set ((s,t)::a') -> printf "%s:=%s\n" (type_to_string s) (type_to_string t); show_assign (Set a')
-  | Compose (a1,a2) -> show_assign a1; printf "######\n"; show_assign a2
+  | Infer.Set [] -> printf ""
+  | Infer.Set ((s,t)::a') -> printf "%s:=%s\n" (Type.type_to_string s) (Type.type_to_string t); show_assign (Set a')
+  | Infer.Compose (a1,a2) -> show_assign a1; printf "######\n"; show_assign a2
 ;;
 
 let _ =
   let exp = Parse.top Lexer.token (Lexing.from_channel stdin) in
-  let t = infer exp in
+  let es = Exp.exp_to_string exp in
+  printf "exp: %s\n" es;
+  let t = Infer.infer exp in
+  let ts = Type.type_to_string t in
+  printf "type: %s\n" ts;
   let v = Eval.eval [] exp in
-  let es = exp_to_string exp in
-  let ts = type_to_string t in
   let vs = Value.value_to_string v in
-  printf "exp: %s\ntype: %s\nvalue: %s\n" es ts vs
+  printf "value: %s\n" vs
 ;;
