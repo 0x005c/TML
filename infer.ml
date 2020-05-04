4200596b3522348bee672cd4e291595e6b5f3d1f
0x005c <52650857+0x005c@users.noreply.github.com>
2020-05-04 22:33:45 +0900

    Fix type inference

--- infer.ml
+++ infer.ml
@@ -50,36 +50,36 @@ let rec lookupG gamma s =
                       else lookupG gamma' s
 ;;
 
-let rec inferC gamma c e =
+let rec inferC gamma e =
   match e with
-  | Exp.Int _ -> Type.Int
-  | Exp.Float _ -> Type.Float
+  | Exp.Int _ -> (Type.Int,[])
+  | Exp.Float _ -> (Type.Float,[])
   | Exp.IAdd (e1,e2) | Exp.ISub (e1,e2) | Exp.IMul (e1,e2) | Exp.IDiv (e1,e2) ->
-      let (t1,t2) = (inferC gamma c e1,inferC gamma c e2) in
-      let c' = (t1,Type.Int)::(t2,Type.Int)::c in
-      let asgn = unify(c') in
-      if assign asgn t1 == assign asgn t2 then Type.Int
-      else raise (InferenceError e)
+      let ((t1,c1),(t2,c2)) = (inferC gamma e1,inferC gamma e2) in
+      let c1 = (t1,Type.Int)::c1 in
+      let c2 = (t2,Type.Int)::c2 in
+      (Type.Int,c1@c2)
   | Exp.FAdd (e1,e2) | Exp.FSub (e1,e2) | Exp.FMul (e1,e2) | Exp.FDiv (e1,e2) ->
-      let (t1,t2) = (inferC gamma c e1,inferC gamma c e2) in
-      let c' = (t1,Type.Float)::(t2,Type.Float)::c in
-      let asgn = unify(c') in
-      if assign asgn t1 == assign asgn t2 then Type.Float
-      else raise (InferenceError e)
+      let ((t1,c1),(t2,c2)) = (inferC gamma e1,inferC gamma e2) in
+      let c1 = (t1,Type.Float)::c1 in
+      let c2 = (t2,Type.Float)::c2 in
+      (Type.Float,c1@c2)
   | Exp.Lambda (s,e) ->
       let t1 = fresh() in
       let gamma' = (s,t1)::gamma in
-      let t2 = inferC gamma' c e in
-      Type.Fun (t1,t2)
+      let (t2,c) = inferC gamma' e in
+      let t1 = assign (unify c) t1 in
+      (Type.Fun (t1,t2),c)
   | Exp.Var s ->
-      lookupG gamma s
+      (lookupG gamma s,[])
   | Exp.Apply (e1,e2) ->
-      (* 型変数は常にユニークに生成されるため、C1, C2に分けなくていい（はず） *)
-      let (t1,t2) = (inferC gamma c e1,inferC gamma c e2) in
+      let ((t1,c1),(t2,c2)) = (inferC gamma e1,inferC gamma e2) in
       let x = fresh() in
-      let c' = (t1,Type.Fun (t2,x))::c in
-      let asgn = unify c' in
-      assign asgn x
+      let c' = (t1,Type.Fun (t2,x))::c1@c2 in
+      (x,c')
 ;;
 
-let infer e = inferC [] [] e ;;
+let infer e =
+  let (t,c) = inferC [] e in
+  assign (unify c) t
+;;
