5287f6aebf5b53a2d5baaab325a400d0a18a2750
0x005c <52650857+0x005c@users.noreply.github.com>
2020-05-10 07:44:32 +0900

    Remove confusing comment

--- builtin.ml
+++ builtin.ml
@@ -1,7 +1,6 @@
 let builtin_i2f v = Value.Float (float_of_int (Value.get_i v)) ;;
 let builtin_f2i v = Value.Int (int_of_float (Value.get_f v)) ;;
 
-(* Eval.evalも変更すること *)
 let builtin_table =
   [ ("i2f", Type.Fun (Type.Int, Type.Float), Value.Builtin builtin_i2f)
   ; ("f2i", Type.Fun (Type.Float, Type.Int), Value.Builtin builtin_f2i) ]
