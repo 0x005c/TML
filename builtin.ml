a72129eb3dfe1e3b76dd8abb989bebe50b92e8d2
0x005c <52650857+0x005c@users.noreply.github.com>
2020-05-10 19:57:15 +0900

    Add builtin print and println function

--- builtin.ml
+++ builtin.ml
@@ -3,13 +3,17 @@ let builtin_f2i v = Value.Int (int_of_float (Value.get_f v)) ;;
 let builtin_print_i v = Printf.printf "%d\n" (Value.get_i v) ; Value.Unit ;;
 let builtin_print_f v = Printf.printf "%f\n" (Value.get_f v) ; Value.Unit ;;
 let builtin_print_b v = Printf.printf "%b\n" (Value.get_b v) ; Value.Unit ;;
+let builtin_print v = Printf.printf "%s" (Value.get_s v) ; Value.Unit ;;
+let builtin_println v = Printf.printf "%s\n" (Value.get_s v) ; Value.Unit ;;
 
 let builtin_table =
   [ ("i2f", Type.Fun (Type.Int, Type.Float), Value.Builtin builtin_i2f)
   ; ("f2i", Type.Fun (Type.Float, Type.Int), Value.Builtin builtin_f2i)
   ; ("print_i", Type.Fun (Type.Int, Type.Unit), Value.Builtin builtin_print_i)
   ; ("print_f", Type.Fun (Type.Float, Type.Unit), Value.Builtin builtin_print_f)
-  ; ("print_b", Type.Fun (Type.Bool, Type.Unit), Value.Builtin builtin_print_b) ]
+  ; ("print_b", Type.Fun (Type.Bool, Type.Unit), Value.Builtin builtin_print_b)
+  ; ("print", Type.Fun (Type.String, Type.Unit), Value.Builtin builtin_print)
+  ; ("println", Type.Fun (Type.String, Type.Unit), Value.Builtin builtin_println) ]
 ;;
 
 let builtin_tenv =
